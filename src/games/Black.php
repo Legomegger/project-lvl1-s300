c6b6ecbfa578bcd865bd862d67d28e36dfcc6ffd
Legomegger <legomegakst@gmail.com>
2018-07-04 21:32:39 +0600

    add gameLogic

--- src/games/Black.php
+++ src/games/Black.php
@@ -4,7 +4,6 @@ namespace BrainGames\Black;
 use function \cli\line;
 use function \cli\prompt;
 
-const END_GAME_FLAG = 3;
 function isNumberEven($number)
 {
     return $number % 2 == 0;
@@ -12,9 +11,11 @@ function isNumberEven($number)
 function main()
 {
     $gameLogic = [];
-    $gameLogic["rules"] = ["Answer \"yes\" if number even, otherwise answer \"no\""];
-    $gameLogic["question"] = rand(1, 15);
-    $gameLogic["correctAnswer"] = isNumberEven($question) ? "yes" : "no";
-    BrainGames\Engine\run($gameLogic);
+    $gameLogic["rules"] = "Answer \"yes\" if number even, otherwise answer \"no\"";
+    for ($i = 0; $i < 3; $i += 1) {
+        $question = rand(1, 15);
+        $gameLogic["question"] = $question;
+        $gameLogic["correctAnswer"] = isNumberEven($question) ? "yes" : "no";
+    }
+    \BrainGames\Engine\run($gameLogic);
 }
-
