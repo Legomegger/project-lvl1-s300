935b91cdd844269d831e7113482201b728434398
Legomegger <legomegakst@gmail.com>
2018-07-05 22:32:13 +0600

    add gcd game

---
+++ src/games/BrainGcd.php
@@ -0,0 +1,26 @@
+<?php
+namespace BrainGames\BrainGcd;
+
+use function \cli\line;
+use function \cli\prompt;
+
+function findGcd ($a, $b) {
+    if ($b === 0) {
+        return $a;
+    } else {
+        return findGcd($b, $a % $b);
+    }
+};
+function run()
+{
+    $gameLogic = function () {
+        $data = [];
+        $data["rules"] = "Find the greatest common divisor of given numbers.";
+        $number1 = rand(1, 200);
+        $number2 = rand(1, 200);
+        $data["question"] = "{$number1} {$number2}";
+        $data["correctAnswer"] = findGcd($number1, $number2);
+        return $data;
+    };
+    \BrainGames\Engine\run($gameLogic);
+}
