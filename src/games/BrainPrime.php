c3ceb4e46bc4de78c5bed6a8b4d01a9da3c4a390
Legomegger <legomegakst@gmail.com>
2018-07-07 08:23:12 +0600

    add prime game

---
+++ src/games/BrainPrime.php
@@ -0,0 +1,31 @@
+<?php
+namespace BrainGames\BrainPrime;
+
+use function \cli\line;
+use function \cli\prompt;
+use function \BrainGames\Engine\runGame;
+
+const RULE = "Is number prime? Answer \"yes\" or \"no\"";
+
+function run()
+{
+
+    $isPrime = function ($num) {
+        for ($i = 2; $i < $num; $i += 1) {
+            if ($num % $i == 0) {
+                return false;
+            }
+            return true;
+        }
+    };
+
+    $getGameData = function () use ($isPrime) {
+        $data = [];
+        $question = rand(1, 100);
+        $data["question"] = "{$question}";
+        $data["correctAnswer"] = $isPrime($question) ? "yes" : "no";
+        return $data;
+    };
+
+    runGame($getGameData, RULE);
+}
